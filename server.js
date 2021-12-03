1914b09811c3fb5619ccf191d2a6996bc93176ae
BahamJustin <bahamjustin@outlook.com>
2021-12-03 04:02:39 -0600

    heroku

---
+++ server.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const { notes } = require("./data/notes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/notes.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
+
+// GET /api/notes - read db.json and return all saved notes as json
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+// POST /api/notes -  recieve new note, add to db.json, return new note --- give each note a unique id, look into npm packages
+app.post("/api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});
+
+// DELETE /api/notes/:id - read all notes, remove note qith given id, rewrite all anotes to db.json
+
+// GET /notes - return notes file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
